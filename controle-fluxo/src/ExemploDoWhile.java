170e3a9d1721cb99d092c2121834efc99b6cd033
nilton <nilferraz@gmail.com>
2025-04-15 08:00:14 -0300

    novos exercicios

---
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -0,0 +1,6 @@
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        int numero = 1; for (int x=1; x<2; x++){ numero = numero + x; } System.out.println("O valor de número é: " + numero); }
+    }
